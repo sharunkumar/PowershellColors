148256c55a0232ab9609db754ab01526136da1b6
Sharun <sharunksplus@gmail.com>
2022-05-29 22:59:39 +0530

    Add project files.

---
+++ PowershellColors/Class1.cs
@@ -0,0 +1,35 @@
+﻿namespace PowershellColors
+{
+    public class ColorPool
+    {
+        private List<uint> _colors;
+        private int _index;
+
+        public ColorPool()
+        {
+            _colors = new List<uint>();
+
+            for (uint i = 0; i < 16; i++)
+            {
+                _colors.Add(i);
+            }
+        }
+
+        public ColorPool(params uint[] excludedColors) : this()
+        {
+            _colors = _colors.Except(excludedColors).ToList();
+        }
+
+        public int Count() => _colors.Count();
+
+        public uint GetNextColor()
+        {
+            if (_index >= _colors.Count())
+            {
+                _index = 0;
+            }
+
+            return _colors[_index++];
+        }
+    }
+}
---
+++ PowershellColors/MappedColorPool.cs
@@ -0,0 +1,35 @@
+﻿namespace PowershellColors
+{
+    public class MappedColorPool<T> : ColorPool where T : notnull
+    {
+        private readonly Dictionary<T, uint> _mapping;
+
+        public MappedColorPool() : base()
+        {
+            _mapping = new();
+        }
+
+        public MappedColorPool(params uint[] excludedColors) : base(excludedColors)
+        {
+            _mapping = new();
+        }
+
+        public uint GetColor(T key)
+        {
+            if (!_mapping.ContainsKey(key))
+                _mapping.Add(key, this.GetNextColor());
+
+            return _mapping[key];
+        }
+
+        public uint this[T key]
+        {
+            get => GetColor(key);
+        }
+
+        public void FlushMappings()
+        {
+            _mapping.Clear();
+        }
+    }
+}
